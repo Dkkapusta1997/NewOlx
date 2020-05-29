164d6bc737d8aeec63c602b4abd8c627e83e6f64
Kapi <Dkkapusta1997@yahoo.pl>
2020-05-29 15:02:02 +0200

    Add models DB

---
+++ db/connection.js
@@ -0,0 +1,10 @@
+
+module.exports={
+    client: 'mysql',
+    connection:{
+        host: 'remotemysql.com',
+        user: 'jIJ11IMxcb',
+        password: 'Rb3Pew8olC',
+        database: 'jIJ11IMxcb'
+    }
+}
---
+++ db/model/Account.js
@@ -0,0 +1,5 @@
+export const Account={
+    id: {type: Number},
+    email: {type: String},
+    password:{type: String}
+}
